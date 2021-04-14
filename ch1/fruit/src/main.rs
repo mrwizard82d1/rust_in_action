a898578480873246adff2efb7b3ed5333a9a086f
Larry Jones <larry.jones@reveal-energy.com>
2021-04-14 17:59:57 -0500

    Rust panics at access outside vector bounds
    Previous commits described a panic resulting from the call to
    `assert_eq!`. If `assert_eq!` is supplied with two unequal values, it
    will cause a panic even **without** a buffer overfow.
    
    This commit causes a panic by attempting to access a value outside the
    bounds of a vector.

--- ch1/fruit/src/main.rs
+++ ch1/fruit/src/main.rs
@@ -5,5 +5,8 @@ fn main() {
     let buffer_overflow = fruit[4];
 
     // Macro asserts that two expressions have equal values. Causes panic if false.
-    assert_eq!(buffer_overflow, '🍉') // <2>
+    // assert_eq!(buffer_overflow, '🍉') // <2>
+    // Even though `assert_eq!` causes a panic on failure, simply accessing a value outside the
+    // bounds of an array produces a panic without the assertion.
+    println!("{}", buffer_overflow);
 }
