038de9c636363810a15aee29de54cbcd7188f783
Larry Jones <larry.jones@reveal-energy.com>
2021-04-15 17:14:27 -0500

    `into_iter` against a `Vector` yields values
    Previous commits invoked `into_iter` on an array.
    
    This [Stack Overflow post](https://stackoverflow.com/questions/34733811/what-is-the-difference-between-iter-and-into-iter)
    states "Arrays implement `IntoIterator` (in such a surprising fashion)
    to make it possible to iterate over references to them in `for` loops."
    
    This commit changes the type of `haystack` from `Array` to `Vector`.
    Then everything works "as advertised."

--- ch2/catalan-numbers/src/main.rs
+++ ch2/catalan-numbers/src/main.rs
@@ -6,7 +6,15 @@ fn main() {
     // 15 |         if item == needle {
     //    |                 ^^ no implementation for `&{integer} == {integer}`
     //
-    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796];
+    // Based on this
+    // [Stack Overflow Post](https://stackoverflow.com/questions/34733811/what-is-the-difference-between-iter-and-into-iter),
+    // I discovered that **Arrays** implement `into_iter` "(in such a surprising fashion) to make
+    // it possible to iterate over references to them in `for` loops."
+    //
+    // When I checked the book, I discovered that it actually did not declare `haystack` to be an
+    // `Array` but a `Vector`!
+    // let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796];
+    let haystack = vec![1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796];
 
     // A call to `into_iter()` iterates over its collection but returns values of the collection
     // **by value** instead of by reference.
