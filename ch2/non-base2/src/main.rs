4831ee26ef0227920bb1d6f3ee96d22b180e0c37
Larry Jones <larry.jones@reveal-energy.com>
2021-04-15 09:50:25 -0500

    Align output to better communicate

--- ch2/non-base2/src/main.rs
+++ ch2/non-base2/src/main.rs
@@ -3,11 +3,11 @@ fn main() {
     let thirty = 0o36; // octal literal
     let three_hundred = 0x12C; // and hexadecimal literal
 
-    println!("base 10: {} {} {}", three, thirty, three_hundred);
+    println!("base 10: {:9} {:9} {:9}", three, thirty, three_hundred);
     // Output as binary
-    println!("base 2: {:b} {:b} {:b}", three, thirty, three_hundred);
+    println!("base  2: {:9b} {:9b} {:9b}", three, thirty, three_hundred);
     // Output as octal
-    println!("base 8: {:o} {:o} {:o}", three, thirty, three_hundred);
+    println!("base  8: {:9o} {:9o} {:9o}", three, thirty, three_hundred);
     // Output as hexadecimal (small letters)
-    println!("base 16: {:x} {:x} {:x}", three, thirty, three_hundred);
+    println!("base 16: {:9x} {:9x} {:9x}", three, thirty, three_hundred);
 }
