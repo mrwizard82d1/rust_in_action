4ee068cf1bc0efa05c94ac21a3068591bd471a98
Larry Jones <larry.jones@reveal-energy.com>
2021-04-15 20:55:45 -0500

    Add comments (forgotten in last commit)

--- ch2/match-needles/src/main.rs
+++ ch2/match-needles/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    // let needle = 42;
+    // let needle = 42; // the variable needle is now redundant
     let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
 
     for item in &haystack {
+        // The `match` expression returns a value - which can then be bound to a variable.
         let result = match item {
-            42 | 132 => "hit!",
-            _ => "miss!",
+            42 | 132 => "hit!", // Eureka! Found a match.
+            _ => "miss!", // The underscore is a "wildcard" that matches any value
         };
 
         if result == "hit!" {
