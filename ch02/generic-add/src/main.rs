ede41d3707c159e2b867a2781662da62607be4f1
Larry Jones <larry.jones@reveal-energy.com>
2021-04-16 20:41:42 -0500

    Type of add argument must support `Add`

--- ch02/generic-add/src/main.rs
+++ ch02/generic-add/src/main.rs
@@ -1,7 +1,11 @@
-fn add<T>(i: T, j: T) -> T {
+// The fragment `<T: std::ops::Add<Output = t>>` requires that T must implement `Add` (support
+// the `Add` trait?) and that the result must be of the same type as the input.
+fn add<T: std::ops::Add<Output = T>>(i: T, j: T) -> T {
     i + j
 }
 
 fn main() {
-    println!("Hello, world!");
+    let sum = add(10, 20);
+
+    println!("{}", sum);
 }
